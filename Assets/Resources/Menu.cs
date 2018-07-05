f74d87f37985d7ff93dd7878196eb58eb4246d81
Jacob Logas <logasja@gatech.edu>
2018-07-05 15:01:19 -0400

    Add sliding menu for configuration

---
+++ Assets/Resources/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	RectTransform panelTrans;
+
+	void Start() {
+		panelTrans = GetComponent<RectTransform>();
+	}
+
+	public void movePanelIn() {
+		panelTrans.offsetMin = new Vector2(0, panelTrans.offsetMin.y);
+		panelTrans.offsetMax = new Vector2(0, panelTrans.offsetMax.y);
+	}
+
+	public void movePanelOut() {
+		panelTrans.offsetMin = new Vector2(-400, panelTrans.offsetMin.y);
+		panelTrans.offsetMax = new Vector2(-400, panelTrans.offsetMax.y);
+	}
+}
