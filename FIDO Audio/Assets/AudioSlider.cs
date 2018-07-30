68a812b9ead03b8a71422ed2b92da43cc8f36f95
Will Mitchell <ChangedNameTo@users.noreply.github.com>
2018-07-29 22:27:20 -0400

    Audio test working

---
+++ FIDO Audio/Assets/AudioSlider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class AudioSlider : MonoBehaviour {
+
+    public Slider mainSlider;
+    public AudioSource sineTone;
+
+    float currValue;
+
+    public void Start()
+    { 
+    	sineTone = GetComponent<AudioSource>();
+        mainSlider.onValueChanged.AddListener(delegate {ValueChangeCheck(); });
+	    currValue = mainSlider.value;
+    }
+
+    public void ValueChangeCheck()
+    {
+        Debug.Log(mainSlider.value);
+        if(currValue != mainSlider.value) {
+	    	sineTone.Play(0);
+        }
+        currValue = mainSlider.value;
+    }
+}
